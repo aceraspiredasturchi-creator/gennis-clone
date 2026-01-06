0249cd3398b7562bced6b83fb22ac3c7c47aeb02
Erlan Amrakulov <aceraspiredasturchi@gmail.com>
2026-01-06 15:40:18 +0500

    add code

---
+++ registers/login/login.js
@@ -0,0 +1,20 @@
+const my_username = "Erlan";
+const my_parol = "12345";
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const error = document.getElementById('Error');
+
+    console.log(username, password);
+
+    if (username === my_username && password === my_parol) {
+        console.log("Kirish muvafaqiyatli");
+    }
+    else {
+        error.textContent = "Ism yoki parol xato kiritildi";
+        error.style.color = "red";
+    }
+
+}
